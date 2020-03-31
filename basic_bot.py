9ce8e0f1b38419612ff06bae2d012b0d89ef3539
Raphaël Langella <raphael.langella@gmail.com>
2020-04-01 00:17:30 +0200

    Simplify mana dice code
    Also assume emojis with name matching the files in the vassal module

--- basic_bot.py
+++ basic_bot.py
@@ -4,21 +4,6 @@ import os
 import random
 import sys
 
-red_die_url = "https://cf.geekdo-images.com/original/img/8vtZaagQ2VJFtSqcoEBLXqNSK38=/0x0/pic1554401.jpg"
-green_die_url = "https://cf.geekdo-images.com/original/img/707hBm7V_9kfCRLTYilZhrL1OgM=/0x0/pic1554402.jpg"
-blue_die_url = "https://cf.geekdo-images.com/original/img/W4gA5CFcVgcnIIZ46wnFMj0W9gw=/0x0/pic1554404.jpg"
-white_die_url = "https://cf.geekdo-images.com/original/img/V4r-VJ947Mgt3BMNU_e8GT8uhPM=/0x0/pic1554399.jpg"
-black_die_url = "https://cf.geekdo-images.com/original/img/n426rPLtbuAQQx2QxAoCkmkq6Uc=/0x0/pic1554405.jpg"
-gold_die_url = "https://cf.geekdo-images.com/original/img/LE_fBgkU_nU5GfmHpLpYONvc78A=/0x0/pic1554403.jpg"
-
-number_url_map = {\
-    1:red_die_url,\
-    2:green_die_url,\
-    3:blue_die_url,\
-    4:white_die_url,\
-    5:black_die_url,\
-    6:gold_die_url}
-
 description = '''An example bot to showcase the discord.ext.commands extension
 module.
 
@@ -31,27 +16,10 @@ async def on_ready():
     print(bot.user.name)
     print(bot.user.id)
     print('------')
+    bot.mana_dice = []
     for e in bot.emojis:
-        if e.name == "red_die":
-            bot.red_emoji = e
-        if e.name == "green_die":
-            bot.green_emoji = e
-        if e.name == "blue_die":
-            bot.blue_emoji = e
-        if e.name == "white_die":
-            bot.white_emoji = e
-        if e.name == "black_die":
-            bot.black_emoji = e
-        if e.name == "gold_die":
-            bot.gold_emoji = e
-    
-    bot.die_emoji_map = {\
-    1:bot.red_emoji,\
-    2:bot.green_emoji,\
-    3:bot.blue_emoji,\
-    4:bot.white_emoji,\
-    5:bot.black_emoji,\
-    6:bot.gold_emoji}
+        if e.name.startswith('die_'):
+            bot.mana_dice.append(e)
 
 @bot.command()
 async def add(ctx, left: int, right: int):
@@ -71,12 +39,11 @@ async def roll(ctx, dice: str):
     await ctx.send(result)
 
 @bot.command()
-async def mana(ctx, count: int):
+async def mana(ctx, count: int = None):
     """Rolls a mana dice."""
-    # result = random.randint(1,6)
-    # die_embed = discord.Embed().set_thumbnail(url=number_url_map[result])
-    # await ctx.send(embed=die_embed)
-    result = ', '.join(str(bot.die_emoji_map[random.randint(1, 6)]) for r in range(count))
+    if not count:
+        count = 1
+    result = ' '.join(str(random.choice(bot.mana_dice)) for r in range(count))
     await ctx.send(result)
 
 @bot.command(description='For when you wanna settle the score some other way')
